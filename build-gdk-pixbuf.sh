4fba9b8cc06e00c2b89d15060308bfe49ae3b535
Yusuke Ishida <yusuke.ishida.gnl@gmail.com>
2015-10-29 15:53:03 +0900

    modify including loader of gdk ( to be loadable .png file )

--- build-gdk-pixbuf.sh
+++ build-gdk-pixbuf.sh
@@ -2,3 +2,7 @@ libname=gdk-pixbuf-2.32.1
 filename=${libname}.tar.xz
 url=http://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/2.32/${filename}
 
+configure() {
+	./configure ${configure_flags} --with-included-loaders
+}
+
