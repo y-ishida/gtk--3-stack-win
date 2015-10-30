546a298fa0a91510bb3cc0a53dbf30242807fbc0
Yusuke Ishida <yusuke.ishida.gnl@gmail.com>
2015-10-30 15:10:58 +0900

    fix it that isn't able to build libgee by cleaning .c files

--- build-gee.sh
+++ build-gee.sh
@@ -2,3 +2,8 @@ libname=libgee-0.18.0
 filename=${libname}.tar.xz
 url=https://download.gnome.org/sources/libgee/0.18/${filename}
 
+clean() {
+	# don't clean that couse to be removed .c files
+	:
+}
+
