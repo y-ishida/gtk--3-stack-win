1d8574054c10fd52d2637e9045a883dc6a12b0d2
Yusuke Ishida <yusuke.ishida@gridraw.com>
2016-04-03 21:35:12 +0000

    Modify to disable X and etc.
    This modification is needed to building on Arch Linux.

--- build-cairo.sh
+++ build-cairo.sh
@@ -2,3 +2,12 @@ libname=cairo-1.14.2
 filename=${libname}.tar.xz
 url=http://www.cairographics.org/releases/${filename}
 
+configure() {
+	./configure ${configure_flags} \
+		--disable-xlib \
+		--disable-xlib-xrender \
+		--disable-xcb \
+		--disable-xlib-xcb \
+		--disable-xcb-shm \
+		CFLAGS="-Wno-error=implicit-function-declaration"
+}
--- build-gtk.sh
+++ build-gtk.sh
@@ -4,9 +4,41 @@ url=http://ftp.gnome.org/pub/gnome/sources/gtk+/3.16/${filename}
 
 configure() {
 	./configure ${configure_flags} \
-		--with-included-immodules \
+		--disable-dependency-tracking \
+		--disable-debug \
 		--disable-installed-tests \
-		--enable-win32-backend
+		--disable-xkb \
+		--disable-xinerama \
+		--disable-xrandr \
+		--disable-xfixes \
+		--disable-xcomposite \
+		--disable-xdamage \
+		--disable-x11-backend \
+		--enable-win32-backend \
+		--disable-quartz-backend \
+		--disable-broadway-backend \
+		--disable-wayland-backend \
+		--disable-mir-backend \
+		--disable-quartz-relocation \
+		--disable-cups \
+		--disable-papi \
+		--disable-cloudprint \
+		--disable-test-print-backend \
+		--disable-schemas-compile \
+		--disable-introspection \
+		--disable-colord \
+		--disable-libcanberra \
+		--disable-gtk-doc \
+		--disable-gtk-doc-html \
+		--disable-gtk-doc-pdf \
+		--disable-man \
+		--disable-doc-cross-references \
+		--without-x \
+		--with-included-immodules
+
+#		--with-included-immodules \
+#		--disable-installed-tests \
+#		--enable-win32-backend
 }
 
 build() {
